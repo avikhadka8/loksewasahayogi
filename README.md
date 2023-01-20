a39d51f6a921204966faafae65a4a5c4e01ea5f3
avikhadka8 <khadkaavinash8@gmail.com>
2023-01-20 12:25:16 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# loksewasahayogi.github.io
---
+++ teaching_blog/app_users/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from app_users import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+# app_name = 'app_users'
+urlpatterns = [
+    path('',views.HomeView.as_view(),name='index'),
+    path('register/', views.register, name='register'),
+    path('user_login/', views.user_login, name='user_login'),
+    path('logout/', views.user_logout, name='user_logout'),
+    path('contact/', views.ContactView.as_view(), name="contact"),
+    # path('search/', views.search, name="search"),
+]
+
+
